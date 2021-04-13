dfb26637e5e94ae12ece175e349d72d2665288a1
Lin Huan Po <foxs91092@gmail.com>
2021-04-13 22:02:03 +0900

    1498C changed to iterative version to avoid potential stackoverflow (Original is accepted on CF already)

--- CustomizedPractice/RandomPractice/1498C.cpp
+++ CustomizedPractice/RandomPractice/1498C.cpp
@@ -26,34 +26,6 @@ using VS = vector <string>;
 using VC = vector <char>;
 const long long MOD = 1000000000 + 7;
 
-long long solve(VVVLL& dp, int now, int k, int dir) {
-    int B = dp.size()-1;
-
-    if (now == 0 && dir == 1) {
-        return 0;
-    }
-
-    if (now == B && dir == 0) {
-        return 0;
-    }
-
-    if (dp[now][k][dir] != -1) {
-        return dp[now][k][dir];
-    }
-
-    if (k <= 1) {
-        return dp[now][k][dir] = 0;
-    }
-
-    int vdir = dir == 0 ? 1 : -1;
-    dp[now][k][dir] = 0;
-    long long ret = solve(dp, now + vdir, k, dir);
-    ret += solve(dp, now, k-1, dir^1) + 1;
-    ret %= MOD;
-
-    return dp[now][k][dir] = ret;
-}
-
 int main()
 {
     int TC;
@@ -65,8 +37,19 @@ int main()
         int K;
         cin >> N >> K;
 
-        VVVLL dp(N+1, VVLL(K+1, VLL(2, -1)));
-        cout << solve(dp, 0, K, 0)+1 << endl;
+        VVVLL dp(K+1, VVLL(N+1, VLL(2)));
+
+        for (int k = 2; k <= K; ++k) {
+            for (int j = N-1; j >= 0; --j) {
+                dp[k][j][0] = (dp[k][j+1][0] + dp[k-1][j][1] + 1) % MOD;
+            }
+
+            for (int j = 1; j <= N; ++j) {
+                dp[k][j][1] = (dp[k][j-1][1] + dp[k-1][j][0] + 1) % MOD;
+            }
+        }
+
+        cout << dp[K][0][0]+1 << endl;
     }
 
     return 0;
